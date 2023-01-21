7e7fec1ec1a08f0f1aaf50a66929c86f216c607d
som <githubsom@gmail.com>
2023-01-21 21:07:26 +0530

    initial commit: added .gitignore and basic react-app stuff

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Firstcomponent from "./components/Firstcomponent";
+
+const App = () => {
+  return (
+   
+    <div> <Firstcomponent></Firstcomponent>
+      <h2>Let's get started!</h2>
+      <p>Fuck you if your mad</p>
+      <h1>IIIT Pune</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Firstcomponent.js
@@ -0,0 +1,9 @@
+const Firstcomponent = () => {
+  return (
+    <div>
+      <h1>My name is som singh lodhi</h1>
+      <p> Please support me</p>
+    </div>
+  );
+};
+export default Firstcomponent;
